4082a43d80b0967d9a909a6045f85bbde2f7ca76
AlokGJ <alok88joshi@gmail.com>
2021-04-20 12:23:41 +0530

    Added post install build

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js'
+        filename: 'main.js',
+        libraryTarget: 'umd',
+        library: 'FEDERATE'
     },
     mode: 'development',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
